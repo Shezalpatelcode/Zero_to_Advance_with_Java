a641364c4d115c2f5ebfb31f542a9ce6cb7cfc00
Shezalpatelcode <sheezalp88@gmail.com>
2026-03-15 00:01:00 +0530

    Simple concepts with Java

---
+++ JavaPractice.java
@@ -0,0 +1,23 @@
+public class JavaPractice
+{
+    int arr[][];
+    int m;
+    int n;
+    public JavaPractice(int mm , int nn)
+    m=mm;
+    n=nn;
+    arr=new int[m][n];
+    //-----------------
+    public void fill_Array()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number of elemnts of the array");
+        for (int i=0i<m;i++){
+            for(int j=0;j<n;j++){
+                 arr[m][n]=in.nextInt();
+            }
+        }
+
+        public void print_Arra
+    }
+}
--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -1,36 +1,78 @@
-package Sorting;
 
+// public class Bubblesort {
+
+//     public static void bubble_sort(int arr[]){
+
+//      for(int turn =0;turn<arr.length-1;turn++){
+//         int swap=0;
+
+//         for(int j=0;j<arr.length-1-turn ;j++){
+//             if(arr[j]>arr[j+1]){
+//                int temp=arr[j];
+//                arr[j]=arr[j+1];
+//                arr[j+1]=temp;
+//                swap++;
+//             }
+//               if(swap==0){
+//             System.out.println("Array is already sorted");
+//         }
+//         }
+//      }
+// } 
+
+// //[printing the array]
+//   public static void printArr(int arr[]){
+//     for(int i=0;i<arr.length;i++){
+//         System.out.print(arr[i]+ " ");
+//     }
+//   }
+//     public static void main(String[] args) {
+//         int arr[]={5,4,1,3,2};
+//         System.out.println();
+//         bubble_sort(arr);
+//         printArr(arr);
+//     }
+// }
+package Sorting;
+import java.util.*;
+//Program to do the Sorting
 public class Bubblesort {
 
-    public static void bubble_sort(int arr[]){
-
-     for(int turn =0;turn<arr.length-1;turn++){
-        int swap=0;
-
-        for(int j=0;j<arr.length-1-turn ;j++){
-            if(arr[j]>arr[j+1]){
-               int temp=arr[j];
-               arr[j]=arr[j+1];
-               arr[j+1]=temp;
-               swap++;
+    //Function written to perform bubble sort
+    public static void bubblesort(int arr[]){
+        for(int turn =0;turn <arr.length-1;turn++){
+          int swap=0;
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    //Swap in that condition
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp; 
+                    }
+                    // swap++;
+                    // if(swap==0){
+                    //   System.out.println("Array is already sorted");
+                    // }
+                    // else{
+                    //   System.out.println("Array is Needed to be Sorted");
+                    // }
+                }
+                if(swap==0){
+                      System.out.println("Array is already sorted");
+                    }
+                    else{
+                      System.out.println("Array is Needed to be Sorted");
+                    }
             }
-              if(swap==0){
-            System.out.println("Array is already sorted");
         }
-        }
-     }
-} 
-
-//[printing the array]
-  public static void printArr(int arr[]){
+public static void printArr(int arr[]){
     for(int i=0;i<arr.length;i++){
         System.out.print(arr[i]+ " ");
     }
-  }
-    public static void main(String[] args) {
-        int arr[]={5,4,1,3,2};
-        System.out.println();
-        bubble_sort(arr);
-        printArr(arr);
-    }
+}
+public static void main(String args[]){
+    int arr[]={5,4,1,3,2};
+    bubblesort(arr);
+    printArr(arr);
+ } 
 }
