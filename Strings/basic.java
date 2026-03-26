ea850ff095c99aeba193e2bf8c49bba165fd610c
Shezalpatelcode <sheezalp88@gmail.com>
2026-03-26 23:32:59 +0530

    your message

--- Strings/basic.java
+++ Strings/basic.java
@@ -3,6 +3,27 @@ package Strings;
 import java.net.SocketPermission;
 import java.util.Scanner;
 public class basic {
+
+    //Print the characters of the array..
+    // public static void printLetters(String str){
+    //     for(int i=0;i<str.length();i++){
+    //         System.out.print(str.charAt(i) +" ");
+    //     }
+    // }
+
+
+    //Print whether it is a palindrome or not
+    public static boolean isPalindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n =str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                //Palindrome
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static void main(String []args){
          //char arr[]={'a','b','c','d'};
          //String str="Captain Sheezal";
@@ -20,17 +41,24 @@ public class basic {
          // which may include the spaces
          //System.out.println(name);
 
-         String fullName="Lietunant Sheezal";
-         System.out.println(fullName.length());
+        //  String fullName="Lietunant Sheezal";
+        //  System.out.println(fullName.length());
         
          //Concatenation of Strings
-            String firstName="Lietunant";
-            String lastName="Sheezal";
-            String fullName1= firstName + " " + lastName;
-            System.out.println(fullName1);
+            // String firstName="Lieutnant";
+            // String lastName="Sheezal";
+            // String fullName1= firstName + " " + lastName;
+            // System.out.println(fullName1);
 
         //charAt() method
-        System.out.println(fullName1.charAt(0));
+        // System.out.println(fullName1.charAt(0));
+        // printLetters(fullName1);
+
+        //Palindrome.....
+        String str="moom";
+        System.out.println(isPalindrome(str));
+
+        
 
     }
 }
